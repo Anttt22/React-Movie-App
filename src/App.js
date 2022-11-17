33e9d1db2c6a1bcc471bad8316fc32fb5daecd7c
home <home@homes-MacBook-Air.local>
2022-11-17 22:06:13 +0200

    POST and Get with FireBase..

--- src/App.js
+++ src/App.js
@@ -1,39 +1,97 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
-  const[movieData, setMovieData]=useState([])
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  function fetchMovieHandler(){ 
-    //console.log("hi")  
-    fetch('https://swapi.dev/api/films/').then(response=>{
-       
-    return response.json();    
-    })
-    .then(data=>{
-      //debugger
-      const transormedData=data.results.map(elem=>{
-        return{
-          id:elem.episode_id,
-          title: elem.title,
-          releaseDate: elem.release_date,
-          openingText: elem.opening_crawl
-        }
-      })
-      setMovieData(transormedData)
-    })
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        'https://react-http-a7c23-default-rtdb.europe-west1.firebasedatabase.app/movie.json'
+        );
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const data = await response.json();
+      console.log(data)
+
+      const loadedMovies =[]
+
+      for(const key in data){
+        loadedMovies.push(
+          {
+            id: key,
+            title: data[key].title,
+            openingText: data[key].openingText,
+            releaseDate: data[key].releaseDate
+          }
+        ) 
+      }
+
+      // const transformedMovies = data.results.map((movieData) => {
+      //   return {
+      //     id: movieData.episode_id,
+      //     title: movieData.title,
+      //     openingText: movieData.opening_crawl,
+      //     releaseDate: movieData.release_date,
+      //   };
+      // });
+      setMovies(loadedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  async function addMovieHandler(movie) {
+    const response = await fetch('https://react-http-a7c23-default-rtdb.europe-west1.firebasedatabase.app/movie.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+
+    }
+    )
+    const data = await response.json();
+    console.log(data);
+  }
+
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
   }
 
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMovieHandler}>Fetch Movies</button>
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
-        <MoviesList movies={movieData} />
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
